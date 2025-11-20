3083c94571c3e4db4652dd96810dc5462ff8b943
Andrés Quiroga <quiroga3719@gmail.com>
2025-11-20 12:52:23 -0500

    Backend base

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientesModule } from './clientes/clientes.module';
+import { PagosModule } from './pagos/pagos.module';
+import { TransaccionesModule } from './transacciones/transacciones.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [ClientesModule, PagosModule, TransaccionesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
